5abded4ac2cfe1218f5bb1031105bc926678fc57
Yannik Dittmar <email@yannik-dittmar.de>
2022-11-06 14:33:54 +0100

    New author page saves the author

--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -6,11 +6,23 @@ describe "New author page", type: :feature do
     visit new_author_path
   end
 
-  it "have text input for first name, last name, and homepage" do
+  it "should have text input for first name, last name, and homepage" do
     visit new_author_path
 
     expect(page).to have_field('author[first_name]')
     expect(page).to have_field('author[last_name]')
     expect(page).to have_field('author[homepage]')
+  end
+
+  it "should save the author when submitted" do
+    visit new_author_path
+
+    page.fill_in 'author[first_name]', with: 'Alan'
+    page.fill_in 'author[last_name]', with: 'Turing'
+    page.fill_in 'author[homepage]', with: 'http://wikipedia.org/Alan_Turing'
+
+    find('input[type="submit"]').click
+
+    expect(Author.where(first_name: 'Alan', last_name: 'Turing', homepage: 'http://wikipedia.org/Alan_Turing').count).to eq(1)
   end
 end
