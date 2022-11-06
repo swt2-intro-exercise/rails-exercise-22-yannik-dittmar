02d61cba4d1c3abcffe870f14ac1a71d51c5e447
Yannik Dittmar <email@yannik-dittmar.de>
2022-11-06 15:08:05 +0100

    Added new author page link to author index page

--- spec/features/author/index_author_spec.rb
+++ spec/features/author/index_author_spec.rb
@@ -9,4 +9,9 @@ describe "Author index page", type: :feature do
         expect(page).to have_text('Name')
         expect(page).to have_text('Homepage')
     end
+
+    it "should have a link to the new author page" do
+        visit authors_path
+        expect(page).to have_link('New', href: new_author_path)
+    end
 end
