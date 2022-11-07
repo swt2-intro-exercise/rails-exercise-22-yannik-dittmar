bf36516ffe534107636b0be6966765381107163f
Yannik Dittmar <email@yannik-dittmar.de>
2022-11-07 16:04:35 +0100

    Added year filter to pages index

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -4,4 +4,6 @@ class Paper < ApplicationRecord
     validates :year, presence: true, numericality: { only_integer: true }
 
     has_and_belongs_to_many :authors
+
+    scope :year, ->(year) { where("year = ?", year) if year.present? }
 end
