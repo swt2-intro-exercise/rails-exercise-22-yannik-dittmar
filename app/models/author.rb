b6cdb058f25d91550d5b345f2b1b04e00636cd6d
Yannik Dittmar <email@yannik-dittmar.de>
2022-11-07 11:32:41 +0100

    Added n:m relation between papers and authors

--- app/models/author.rb
+++ app/models/author.rb
@@ -3,6 +3,8 @@ class Author < ApplicationRecord
     validates :last_name, presence: true, length: { minimum: 3 }
     validates :homepage, presence: true, length: { minimum: 3 }
 
+    has_and_belongs_to_many :papers
+
     def name
         "#{first_name} #{last_name}"
     end
